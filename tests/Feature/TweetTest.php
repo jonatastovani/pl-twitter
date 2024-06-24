95859edba71a14c6d930aabb55468ebbc08fe22b
Jônatas Tovani <98124177+jonatastovani@users.noreply.github.com>
2024-06-24 09:34:39 -0300

    tweet feature

---
+++ tests/Feature/TweetTest.php
@@ -0,0 +1,12 @@
+<?php
+
+// test('example', function () {
+//     $response = $this->get('/');
+
+//     $response->assertStatus(200);
+// });
+
+todo('should be able to create a tweet');
+todo('body is required');
+todo('the tweet should have a max length of 140 characters');
+todo('should show the tweet on the timeline');
